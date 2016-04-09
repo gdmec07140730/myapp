d5887a8e81de9a08b0f69fb07a21989ac1fe2804
Serene <310253770@qq.com>
2016-04-09 16:54:05 +0800

    '20160409.1'

---
+++ app/www/js/app.js
@@ -0,0 +1,95 @@
+angular.module('app', ['ionic', 'app.controllers', 'app.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+
+.config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
+
+  // //设置默认返回按钮的文字   
+  // $ionicConfigProvider.backButton.previousTitleText(false).text('返回');  
+  // // 设置全局 $http 超时  
+  //  $httpProvider.interceptors.push('timeoutHttpIntercept');  
+  // // 配置选项卡，让tab在iOS和Android都显示在底部  
+  // $ionicConfigProvider.tabs.position('bottom');  
+  // $ionicConfigProvider.tabs.style('standard');  
+  // //让nav标题在iOS和Android上都居中显示  
+  // $ionicConfigProvider.navBar.alignTitle('center');  
+  // 
+  $ionicConfigProvider.platform.ios.tabs.style('standard'); 
+  $ionicConfigProvider.platform.ios.tabs.position('bottom');
+  $ionicConfigProvider.platform.android.tabs.style('standard');
+  $ionicConfigProvider.platform.android.tabs.position('standard');
+
+
+  $ionicConfigProvider.platform.ios.navBar.alignTitle('center'); 
+  $ionicConfigProvider.platform.android.navBar.alignTitle('center');
+
+
+  $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-reply');
+  $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-reply');        
+
+
+  $ionicConfigProvider.platform.ios.views.transition('ios'); 
+  $ionicConfigProvider.platform.android.views.transition('android');
+
+  $stateProvider
+
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    // .state('tab.chat-detail', {
+    //   url: '/chats/:chatId',
+    //   views: {
+    //     'tab-chats': {
+    //       templateUrl: 'templates/chat-detail.html',
+    //       controller: 'ChatDetailCtrl'
+    //     }
+    //   }
+    // })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+
+  $urlRouterProvider.otherwise('/tab/dash');
+
+});
